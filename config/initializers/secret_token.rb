7d58dc437de84adc20060bc8bc9533d2545c13a4
Preeti Gupta <guptapreeti.india@gmail.com>
2013-03-16 03:10:29 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParentSitter::Application.config.secret_token = '5ec9c32252db21cb588e40210a936a6a36bd74efc08c60492190b4f2c9bd4b7b873a2b8fde1a3c91017acac9ad1f6bce4a4946a72ab1ac88c753ab6e372c23e9'
